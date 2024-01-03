6e6988e60d67e20b795aa5b9f7c51e730144549a
Tiagolopes49 <a23039@esenviseu.net>
2024-01-03 10:29:53 +0000

    exercicio

---
+++ exe10.py
@@ -0,0 +1,11 @@
+def ConverterBase10(ValorBinario):
+    soma=0
+    expoente=len(ValorBinario)-1
+    for posicao in range(len(ValorBinario)):
+        if ValorBinario[posicao]=="1":
+            soma=soma+2**expoente
+        expoente=expoente-1
+    return soma
+
+base10=ConverterBase10("1011")
+print(base10)
