c972f73234592eeaee02aeb3c17c8900738918c3
Tiagolopes49 <a23039@esenviseu.net>
2024-01-08 08:33:58 +0000

    exercicio

---
+++ jogador.py
@@ -0,0 +1,9 @@
+def ficha(nome,golos=0):
+    return ficha
+ 
+ 
+nome=input("Digite o nome do jogador: ")
+golos=int(input("Digite o numero de golos do jogador: "))
+print(f'O {nome} marcou {golos} golos. ')
+ 
+ficha(nome="",golos="")
