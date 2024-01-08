e5161b7e58377099d888b51cc714e8a47d38dfe3
Tiagolopes49 <a23039@esenviseu.net>
2024-01-08 10:41:49 +0000

    exercicio

---
+++ contagem.py
@@ -0,0 +1,13 @@
+#Tiago Lopes Nº28 10ºT
+def crescente(n):
+    for i in range(1,n+1,1):
+        print(i)
+
+def descrescente(n):
+    for i in range(n,0,-1):
+        print(i)
+
+#programa principal
+crescente(5)
+print()
+descrescente(5)
---
+++ positivo_negativo.py
@@ -0,0 +1,8 @@
+def positivo_negativo(n):
+    if n>=0:
+        print("p")
+    else:
+        print("n")
+
+n=int(input("introduza um numero: "))
+positivo_negativo(n)
