dd059a4c1778dc377f77c0e97a1fb1ff847bf0af
Tiagolopes49 <a23039@esenviseu.net>
2024-02-07 10:27:09 +0000

    exercicio

--- dias_semanas.py
+++ dias_semanas.py
@@ -1,4 +1,5 @@
 #Tiago Lopes Nº28 10ºT
 #o programa vai impirmir os dias uteis da semana
-for i in range(2,7):
-    print(f"{i}º-feira")
+for i in range(1,8):
+    dias=input("Introduza o {i}º dia da semana")
+print(dias)
