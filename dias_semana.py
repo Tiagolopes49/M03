8bc0a702613fef0c8b631fa5f1ae8ff5416eb6d6
Tiagolopes49 <a23039@esenviseu.net>
2024-02-07 10:23:35 +0000

    exercicio

--- dias_semana.py
+++
@@ -1,4 +0,0 @@
-#Tiago Lopes Nº28 10ºT
-#o programa vai imprimir os dias uteis da semana
-def dia_semana():
-    pass
---
+++ dias_semanas.py
@@ -0,0 +1,4 @@
+#Tiago Lopes Nº28 10ºT
+#o programa vai impirmir os dias uteis da semana
+for i in range(2,7):
+    print(f"{i}º-feira")
